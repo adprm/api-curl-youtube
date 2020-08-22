545b361d4fe1d9f7dd1411e3f6b881384b1743ed
adprm <aditiyaprmn00@gmail.com>
2020-08-22 15:15:59 +0700

    curl config

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
+<?php
+
+function get_Curl($url) {
+    $curl = curl_init();
+    curl_setopt($curl, CURLOPT_URL, $url);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+    $result = curl_exec($curl);
+    curl_close($curl);
+
+    return json_decode($result, true);
+}
+
+?>
+
 <!doctype html>
 <html lang="en">
   <head>
