2057dac9a968dfcadf78d27a40d2400fc24f9068
adprm <aditiyaprmn00@gmail.com>
2020-08-22 16:10:24 +0700

    button subscribe

--- index.php
+++ index.php
@@ -59,6 +59,7 @@ $tseriesLatestVideo = $result['items'][0]['id']['videoId'];
                                 <div class="col-md-8">
                                     <h5><?= $pewdiepieName; ?><h5>
                                     <p><?= $pewdiepieSubscriber; ?> Subscribers.</p>
+                                    <div class="g-ytsubscribe" data-channelid="UC-lHJZR3Gqxm24_Vd_AJ5Yw" data-layout="default" data-count="default"></div>
                                 </div>
                             </div>
                             <div class="row mt-3 pb-3">
@@ -77,6 +78,7 @@ $tseriesLatestVideo = $result['items'][0]['id']['videoId'];
                                 <div class="col-md-8">
                                     <h5><?= $tseriesName; ?><h5>
                                     <p><?= $tseriesSubscriber; ?> Subscribers.</p>
+                                    <div class="g-ytsubscribe" data-channelid="UCq-Fj5jknLsUf-MWSy4_brA" data-layout="default" data-count="default"></div>
                                 </div>
                             </div>
                             <div class="row mt-3 pb-3">
@@ -100,5 +102,6 @@ $tseriesLatestVideo = $result['items'][0]['id']['videoId'];
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+    <script src="https://apis.google.com/js/platform.js"></script>
   </body>
 </html>
